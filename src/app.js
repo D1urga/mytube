3a0fc57df82712b441c93d199fe00b7fa7647964
unknown <64825962+D1urga@users.noreply.github.com>
2023-12-28 00:13:01 +0530

    changes in app

--- src/app.js
+++ src/app.js
@@ -6,7 +6,12 @@ import { uploadOnCloudinary } from "./utils/cloudinary.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
