18d9a69628bbdb912a5938c3373ed3525c74ba2d
unknown <64825962+D1urga@users.noreply.github.com>
2023-12-27 21:26:31 +0530

    changes in app

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -140,11 +140,9 @@ const logoutUser = asyncHandler(async (req, res) => {
 });
 
 const getCurrentUser = asyncHandler(async (req, res) => {
-  // return res
-  //   .status(200)
-  //   .json(new ApiResponse(200, req.user, "User fetched successfully"));
-
-  res.json({ data: "done" });
+  return res
+    .status(200)
+    .json(new ApiResponse(200, req.user, "User fetched successfully"));
 });
 
 export { registerUser, loginUser, logoutUser, getCurrentUser };
