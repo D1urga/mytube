b9f5a9190e7f5847d7432428cb30303d23424930
unknown <64825962+D1urga@users.noreply.github.com>
2023-12-31 18:51:50 +0530

    first code

--- src/app.js
+++ src/app.js
@@ -8,13 +8,20 @@ const app = express();
 
 app.use(
   cors({
-    origin: true,
     credentials: true,
     origin: "http://localhost:3001/",
     allowedHeaders: true,
     allowCredentials: true,
   })
 );
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
