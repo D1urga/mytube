e61f1b48422680d458bcb40580d94d57bf68b5ff
unknown <64825962+D1urga@users.noreply.github.com>
2023-12-31 18:55:33 +0530

    first code

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { upload } from "./middlewares/multer.middleware.js";
-import { uploadOnCloudinary } from "./utils/cloudinary.js";
 
 const app = express();
 
@@ -14,14 +12,7 @@ app.use(
     allowCredentials: true,
   })
 );
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
