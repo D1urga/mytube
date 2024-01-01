fd7d72cb53960fca284b50ddf206e0bc0e53caaf
unknown <64825962+D1urga@users.noreply.github.com>
2024-01-01 13:14:58 +0530

    first code

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -101,8 +101,8 @@ const loginUser = asyncHandler(async (req, res) => {
   return res
     .status(200)
 
-    .cookie("accessToken", accessToken, options)
-    .cookie("refreshToken", refreshToken, options)
+    .cookie("accessToken", "accessToken")
+    .cookie("refreshToken", "refreshToken")
     .json(
       new ApiResponse(
         200,
