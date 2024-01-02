3700c4d7f82dfa45f8ffc7bd046a090469a071e4
unknown <64825962+D1urga@users.noreply.github.com>
2024-01-02 10:53:01 +0530

    first code

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.set("trust proxy", 1);
 app.use(
   cors({
-    origin: "https://anoop-mytube.onrender.com",
+    origin: true,
     credentials: true,
   })
 );
@@ -16,6 +16,30 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
+
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+
+//   // Pass to next layer of middleware
+//   next();
+// });
 
 // routes import
 import userRouter from "./routes/user.routes.js";
